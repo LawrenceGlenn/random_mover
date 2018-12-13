19ede2809e93c8c5f2b8430e7f25de945b7c42aa
Lawrence Glenn <lawrence.o.glenn@gmail.com>
2018-12-13 00:15:19 -0800

    added a validation function to make sure the selection exsisted and they were transform nodes before running the random_translate function

--- random_mover.py
+++ random_mover.py
@@ -8,37 +8,60 @@ from maya import cmds
 import random
 
 
+def is_selection_valid(sel):
+	'''
+	check to make sure the selection of items in maya are all
+	transform nodes
+	'''
+	result = False
+	# is the selection empty
+	if sel:
+		invaledSelections = []
+		for item in sel:
+			if cmds.nodeType(item) != 'transform':
+				invaledSelections.append(item)
+			# is this transform node
+
+		if invaledSelections:
+			for item in invaledSelections:
+				print 'Object',item,'is not a transform node'
+
+		else:
+			result=True	
+	else:
+		print '>> No object where selected'
+
+	return result
+
 def random_mover_backend(userRanges):
 	'''
 	moves selected object in maya by a random values given
 	in the peramiters of a min/max range by the user
 	userRanges = the dictionary contianing the min/max range of xyz
 	'''
-	sel = cmds.ls(selection=True)
-	for key in userRanges:
-		random_translate(key,userRanges[key]['min'],userRanges[key]['max'],sel)
 	# get the name of the objects we want to affect
+	sel = cmds.ls(selection=True)
+	if is_selection_valid(sel):
+		for key in userRanges:
+			random_translate(key,userRanges[key]['min'],userRanges[key]['max'],sel)
 	
 
-	# get a random number with in a specified range for x,y,z
-
-
-	# apply that random number to the tx,ty,tz
-	for item in sel:
-		cmds.setAttr(item+'.translateX',random.randint(userXMin,userXMax))
-		cmds.setAttr(item+'.translateY', random.randint(userXMin,userXMax))
-		cmds.setAttr(item+'.translateZ', random.randint(userXMin,userXMax))
-		print cmds.getAttr(item+'.translateX')
-
 def random_translate(axis,min,max,sel):
-
+	'''
+	this will apply a random translate value
+	'''
+	# get a random number with in a specified range for x,y,z
+	# apply that random number to the tx,ty,tz
 	for item in sel:
 		cmds.setAttr(item+'.t'+axis,random.randint(min,max))
 
-
+	
 userRanges = {
 'x': {'min': 1,'max': 20},
 'y': {'min': 1,'max': 20},
 'z': {'min': 1,'max': 20}
 }
+# validate data 
+
+
 random_mover_backend(userRanges)
