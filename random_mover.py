f9fbc9c4a0e6b0519974c036fe22cce18756e1cb
Lawrence Glenn <lawrence.o.glenn@gmail.com>
2018-12-12 21:03:56 -0800

    initial commit

---
+++ random_mover.py
@@ -0,0 +1,6 @@
+'''
+Name: scatter script
+Author: Lawrance and Mark
+Description: The point of this script is to be able to move multiple object in a maya
+scene. We can set the amount of the random range in the x,y,z directions per object.
+'''
