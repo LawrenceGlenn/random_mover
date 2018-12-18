31832d701210ab05609456d913820ad391003a02
LawrenceGlenn <Lawrence.O.Glenn@gmail.com>
2018-12-17 18:53:07 -0800

    Update random_mover.py
    Added functionality and user interface for randomizing scale as well

--- random_mover.py
+++ random_mover.py
@@ -46,6 +46,8 @@ def random_mover_backend(userRanges):
 			random_change("t",key,userRanges[key]['min'],userRanges[key]['max'],sel)
 		for key in userRanges['rotate']:
 			random_change("r",key,userRanges[key]['min'],userRanges[key]['max'],sel)
+		for key in userRanges['scale']:
+			random_change("s",key,userRanges[key]['min'],userRanges[key]['max'],sel)
 
 def random_change(changeType,axis,min,max,sel):
 	'''
@@ -76,13 +78,19 @@ def random_mover_UI():
 	cmds.floatFieldGrp('XRotateInput', numberOfFields=2, label='X Rotate min and max', value1=0, value2=0)
 	cmds.floatFieldGrp('YRotateInput', numberOfFields=2, label='Y Rotate min and max', value1=0, value2=0)
 	cmds.floatFieldGrp('ZRotateInput', numberOfFields=2, label='Z Rotate min and max', value1=0, value2=0)
+	cmds.floatFieldGrp('XScaleInput', numberOfFields=2, label='X Rotate min and max', value1=0, value2=0)
+	cmds.floatFieldGrp('YScaleInput', numberOfFields=2, label='Y Rotate min and max', value1=0, value2=0)
+	cmds.floatFieldGrp('ZScaleInput', numberOfFields=2, label='Z Rotate min and max', value1=0, value2=0)
 	cmds.button(label='Randomize!',
-	command ="from random_mover import random_mover; random_mover.random_mover_backend({'translate':{'x':{'min':cmds.floatFieldGrp('XInput',query=True,value1=True),'max': cmds.floatFieldGrp('XTranslateInput',query=True,value2=True)},
-			    'y':{'min':cmds.floatFieldGrp('YInput',query=True,value1=True),'max': cmds.floatFieldGrp('YTranslateInput',query=True,value2=True)},
-			    'z':{'min':cmds.floatFieldGrp('ZInput',query=True,value1=True),'max': cmds.floatFieldGrp('ZTranslateInput',query=True,value2=True)}},
-			    'rotate':{'x':{'min':cmds.floatFieldGrp('XInput',query=True,value1=True),'max': cmds.floatFieldGrp('XRotateInput',query=True,value2=True)},
-			    'y':{'min':cmds.floatFieldGrp('YInput',query=True,value1=True),'max': cmds.floatFieldGrp('YRotateInput',query=True,value2=True)},
-			    'z':{'min':cmds.floatFieldGrp('ZInput',query=True,value1=True),'max': cmds.floatFieldGrp('ZRotateInput',query=True,value2=True)}}})")
+	command ="from random_mover import random_mover; random_mover.random_mover_backend({'translate':{'x':{'min':cmds.floatFieldGrp('XTranslateInput',query=True,value1=True),'max': cmds.floatFieldGrp('XTranslateInput',query=True,value2=True)},
+			    'y':{'min':cmds.floatFieldGrp('YTranslateInput',query=True,value1=True),'max': cmds.floatFieldGrp('YTranslateInput',query=True,value2=True)},
+			    'z':{'min':cmds.floatFieldGrp('ZTranslateInput',query=True,value1=True),'max': cmds.floatFieldGrp('ZTranslateInput',query=True,value2=True)}},
+			    'rotate':{'x':{'min':cmds.floatFieldGrp('XRotateInput',query=True,value1=True),'max': cmds.floatFieldGrp('XRotateInput',query=True,value2=True)},
+			    'y':{'min':cmds.floatFieldGrp('YRotateInput',query=True,value1=True),'max': cmds.floatFieldGrp('YRotateInput',query=True,value2=True)},
+			    'z':{'min':cmds.floatFieldGrp('ZRotateInput',query=True,value1=True),'max': cmds.floatFieldGrp('ZRotateInput',query=True,value2=True)}},
+			    'scale':{'x':{'min':cmds.floatFieldGrp('XScaleInput',query=True,value1=True),'max': cmds.floatFieldGrp('XScaleInput',query=True,value2=True)},
+			    'y':{'min':cmds.floatFieldGrp('YScaleInput',query=True,value1=True),'max': cmds.floatFieldGrp('YScaleInput',query=True,value2=True)},
+			    'z':{'min':cmds.floatFieldGrp('ZScaleInput',query=True,value1=True),'max': cmds.floatFieldGrp('ZScaleInput',query=True,value2=True)}}})")
 
 	#run the window
 	cmds.showWindow( randomMoverWin )
